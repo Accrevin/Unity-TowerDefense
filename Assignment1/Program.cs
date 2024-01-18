7c7b582bec1089163dff655dc8a9536441afa414
Katie <Katieau.x@gmail.com>
2024-01-18 14:16:21 -0800

    Initial commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Maximum number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int Answer = rand.Next(1, num + 1);
+
+            bool runtime = true;
+            while (runtime)
+            {
+                Console.WriteLine($"Guess a number between 1 and {num}");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == Answer)
+                {
+                    Console.WriteLine("Correct guess!");
+                    runtime = false;
+                }
+
+                else if (guess > Answer)
+                {
+                    Console.WriteLine("Guess too high, try again");
+                }
+
+                else if (guess < Answer)
+                {
+                    Console.WriteLine("Guess too low, try again");
+                }
+
+            }
+
+        }
+    }
+}
