c1ed607b15dc9c26dd0c5d0c13f6728ac75a23e1
VFS\var06katie <Katieau.x@gmail.com>
2024-02-01 16:02:19 -0800

    Started race assignment

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -4,35 +4,83 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Maximum number");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Random rand = new Random();
-            int Answer = rand.Next(1, num + 1);
+            //        Console.WriteLine("Enter the Maximum number");
+            //        int num = Convert.ToInt32(Console.ReadLine());
+            //        Random rand = new Random();
+            //        int Answer = rand.Next(1, num + 1);
 
-            bool runtime = true;
-            while (runtime)
+            //        bool runtime = true;
+            //        while (runtime)
+            //        {
+            //            Console.WriteLine($"Guess a number between 1 and {num}");
+            //            int guess = Convert.ToInt32(Console.ReadLine());
+
+            //            if (guess == Answer)
+            //            {
+            //                Console.WriteLine("Correct guess!");
+            //                runtime = false;
+            //            }
+
+            //            else if (guess > Answer)
+            //            {
+            //                Console.WriteLine("Guess too high, try again");
+            //            }
+
+            //            else if (guess < Answer)
+            //            {
+            //                Console.WriteLine("Guess too low, try again");
+            //            }
+
+            //        }
+
+            //    }
+            //}
+
+            string text = "The quick brown fox jumps over the lazy dog";
+            bool run = true;
+            int loop = 0;
+            while (run)
             {
-                Console.WriteLine($"Guess a number between 1 and {num}");
-                int guess = Convert.ToInt32(Console.ReadLine());
-
-                if (guess == Answer)
+                if (loop == 0)
                 {
-                    Console.WriteLine("Correct guess!");
-                    runtime = false;
+                    Console.Clear();
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        Console.Write(text[i]);
+                        Thread.Sleep(100);
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Enter a letter");
+                    loop++;
                 }
 
-                else if (guess > Answer)
+                else
                 {
-                    Console.WriteLine("Guess too high, try again");
-                }
+                    string letterString = Console.ReadLine();
+                    char LetterChar = Convert.ToChar(letterString);
+                    Console.Clear();
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (text[i] == LetterChar)
+                        {
+                            Console.Write(char.ToUpper(text[i]));
+                            Thread.Sleep(100);
+                        }
 
-                else if (guess < Answer)
-                {
-                    Console.WriteLine("Guess too low, try again");
-                }
+                        else
+                        {
+                            Console.Write(text[i]);
+                            Thread.Sleep(100);
+                        }
+                    }
+                    Console.WriteLine();
 
+                    Console.WriteLine("Enter a letter");
+                }
             }
 
+               
+            }
         }
     }
-}
---
+++ Race/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string course = "----..-.-----...--..----|";
+            char PlayerA = 'A';
+            char PlayerB = 'B';
+            char[] TerrainCode = { '-', '.' };
+            string[] Terrain = { "default", "muddy" };
+            int PlayerAPos = 0;
+            int PlayerBPos = 1;
+
+            for (int j = 0; j < course.Length; j++)
+            {
+                bool APrint = false;
+                bool BPrint = false;
+                Console.Clear();
+                for (int i = 0; i < course.Length; i++)
+                {
+                    if (PlayerAPos == i && APrint == false)
+                    {
+                        Console.Write('A');
+                        PlayerAPos++;
+                        APrint = true;
+                    }
+
+                    else if (PlayerBPos == i && BPrint == false)
+                    {
+                        Console.Write('B');
+                        PlayerBPos++;
+                        BPrint = true;
+                    }
+
+                    else
+                    {
+                        Console.Write(course[i]);
+                    }
+                }
+                Thread.Sleep(200);
+            }
+
+
+        }
+    }
+}
