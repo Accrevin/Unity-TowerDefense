6951efa217c3f798ff450f2a1687aa51b14c8fd0
VFS\var06katie <Katieau.x@gmail.com>
2024-02-01 18:33:28 -0800

    Added random chance to move forward on normal terrain

--- Race/Program.cs
+++ Race/Program.cs
@@ -11,25 +11,53 @@
             string[] Terrain = { "default", "muddy" };
             int PlayerAPos = 0;
             int PlayerBPos = 1;
+            bool RunGame = true;
 
-            for (int j = 0; j < course.Length; j++)
+           while (RunGame)
             {
                 bool APrint = false;
                 bool BPrint = false;
                 Console.Clear();
                 for (int i = 0; i < course.Length; i++)
                 {
+
+
+                    //PlayerA Logic.
                     if (PlayerAPos == i && APrint == false)
                     {
                         Console.Write('A');
-                        PlayerAPos++;
+
+                        //Regular Terrain Logic
+                        if (course[PlayerAPos] == '-')
+                        {
+                            Random rand = new Random();
+                            int PlayerMove = rand.Next(1, 5);
+                            if(PlayerMove == 4) 
+                            {
+                                PlayerAPos++;
+                            }
+                        }
+                         
                         APrint = true;
                     }
 
+
+                    //PlayerB Logic.
                     else if (PlayerBPos == i && BPrint == false)
                     {
                         Console.Write('B');
-                        PlayerBPos++;
+                        //Regular Terrain Logic
+                        if (course[PlayerBPos] == '-')
+                        {
+                            Random rand = new Random();
+                            int PlayerMove = rand.Next(1, 5);
+                            if (PlayerMove == 4)
+                            {
+                                PlayerBPos++;
+                            }
+                        }
+
+                        //
                         BPrint = true;
                     }
 
