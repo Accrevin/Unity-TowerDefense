7af26b74f9c6520b90ea6ffbb5b2880b7bf43f64
Katie <Katieau.x@gmail.com>
2024-02-14 22:11:13 -0800

    Finished Race program

--- Race/Program.cs
+++ Race/Program.cs
@@ -1,25 +1,46 @@
-﻿namespace Race
+﻿using System.Net.Quic;
+
+namespace Race
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string course = "----..-.-----...--..----|";
-            char PlayerA = 'A';
-            char PlayerB = 'B';
-            char[] TerrainCode = { '-', '.' };
-            string[] Terrain = { "default", "muddy" };
             int PlayerAPos = 0;
-            int PlayerBPos = 1;
+            int PlayerBPos = 0;
             bool RunGame = true;
 
-           while (RunGame)
+            while (RunGame)
             {
+                //Checks if program has already printed A or B in this loop yet
                 bool APrint = false;
                 bool BPrint = false;
                 Console.Clear();
                 for (int i = 0; i < course.Length; i++)
                 {
+
+                    //Check to see if any of the players have reached the end of the course yet
+
+                    if (PlayerAPos >= course.Length - 1 || PlayerBPos >= course.Length - 1)
+                    {
+                        if (PlayerAPos >= course.Length - 1)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Player A wins!");
+                            RunGame = false;
+                            break;
+                        }
+
+                        else if (PlayerBPos >= course.Length - 1)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Player B wins!");
+                            RunGame = false;
+                            break;
+                        }
+
+                    }
 
 
                     //PlayerA Logic.
@@ -32,12 +53,23 @@
                         {
                             Random rand = new Random();
                             int PlayerMove = rand.Next(1, 5);
-                            if(PlayerMove == 4) 
+                            if (PlayerMove == 4)
                             {
                                 PlayerAPos++;
                             }
                         }
-                         
+
+                        //Muddy Terrain Logic
+                        else if (course[PlayerAPos] == '.')
+                        {
+                            Random rand = new Random();
+                            int PlayerMove = rand.Next(1, 9);
+                            if (PlayerMove == 8)
+                            {
+                                PlayerAPos++;
+                            }
+                        }
+
                         APrint = true;
                     }
 
@@ -57,7 +89,17 @@
                             }
                         }
 
-                        //
+                        //Muddy Terrain Logic
+                        else if (course[PlayerBPos] == '.')
+                        {
+                            Random rand = new Random();
+                            int PlayerMove = rand.Next(1, 9);
+                            if (PlayerMove == 8)
+                            {
+                                PlayerBPos++;
+                            }
+                        }
+
                         BPrint = true;
                     }
 
@@ -65,7 +107,11 @@
                     {
                         Console.Write(course[i]);
                     }
+
                 }
+
+
+                //zzz
                 Thread.Sleep(200);
             }
 
