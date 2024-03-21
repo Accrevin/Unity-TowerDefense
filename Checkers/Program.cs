442cdf680542d244bac853a43f5ddda821a968b4
Katie <Katieau.x@gmail.com>
2024-03-21 02:51:22 -0700

    Got most of it working, ran out of time to implement pieces getting crowned

---
+++ Checkers/Program.cs
@@ -0,0 +1,438 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace Checkers
+{
+    class Piece
+    {
+        public Vector2 Pos;
+        public char Team;
+
+        public Piece(char team)
+        {
+            Team = team;
+        }
+    }
+
+    class program
+    {
+        static void Main()
+        {
+
+            //Dictionary to turn the coordinate letters into numbers
+            Dictionary<char, int> BoardPlaces = new Dictionary<char, int>();
+
+            BoardPlaces.Add('a', 0);
+            BoardPlaces.Add('b', 1);
+            BoardPlaces.Add('c', 2);
+            BoardPlaces.Add('d', 3);
+            BoardPlaces.Add('e', 4);
+            BoardPlaces.Add('f', 5);
+            BoardPlaces.Add('g', 6);
+            BoardPlaces.Add('h', 7);
+
+
+            //keep track of who's turn it is (0 = X, 1 = O)
+            int turn = 0;
+            //8x8 board
+            int boardSize = 8;
+
+            List<Piece> Xpieces = new List<Piece>();
+            List<Piece> Opieces = new List<Piece>();
+
+
+            //Set the starting positions of the 'O' Pieces
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Piece Player = new Piece('O');
+
+
+                    int xPosition = (j * 2 + i % 2 + 1);
+                    if (xPosition >= 8)
+                    {
+                        xPosition = 0;
+                    }
+                    int yPosition = i;
+
+                    Player.Pos = new Vector2(xPosition, yPosition);
+                    Player.Team = 'O';
+                    Opieces.Add(Player);
+
+                }
+            }
+
+
+            //Set the starting positions of the 'X' Pieces
+            for (int i = 7; i >= 5; i--)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Piece Player = new Piece('X');
+
+
+                    int xPosition = (j * 2 + i % 2 + 1);
+                    if (xPosition >= 8)
+                    {
+                        xPosition = 0;
+                    }
+                    int yPosition = i;
+
+                    Player.Pos = new Vector2(xPosition, yPosition);
+                    Player.Team = 'X';
+                    Xpieces.Add(Player);
+                }
+            }
+
+            //initial board display
+            PrintBoard(Xpieces, Opieces, boardSize);
+
+            while (true)
+            {
+      
+                Console.Clear();
+                PrintBoard(Xpieces, Opieces, boardSize);
+                Console.WriteLine();
+
+
+                //'X' Turn
+                if (turn == 0)
+                {
+                    Console.WriteLine("Player X turn");
+                    string moveinput = Console.ReadLine();
+
+                    //split input into selected piece and direction to move in
+                    string[] move = moveinput.Split(' ');
+
+
+
+                    int TargetIndex = -1;
+
+                    //order of input should be letter then number
+                    string movePiece = move[0];
+                    if (movePiece.Length < 2)
+                    {
+                        Console.WriteLine("Invalid move format. Please enter a valid move.");
+                        continue;
+                    }
+
+                    //look up the letter in the dictionary
+                    int Xtarget = BoardPlaces[movePiece[0]];
+
+                    //Take the number from the coordinates
+                    int YTargetSubtract = Convert.ToInt32(movePiece.Substring(1));
+                    //math
+                    int Ytarget = boardSize - YTargetSubtract;
+
+                    //turn the numbers from input into a Vector 2 to compare to the positions of pieces
+                    Vector2 TargetPiecePos = new Vector2(Xtarget, Ytarget);
+
+                    // Find the index of the piece to move
+                    for (int i = 0; i < Xpieces.Count; i++)
+                    {
+                        if (Xpieces[i].Pos == new Vector2(Xtarget, Ytarget))
+                        {
+                            TargetIndex = i;
+                            break;
+                        }
+                    }
+
+                    // Check if the piece was found
+                    if (TargetIndex <= -1)
+                    {
+                        Console.WriteLine("Invalid move. Please enter a valid move.");
+                        continue;
+                    }
+
+                    //select the piece
+                    Piece TargetMovePiece = Xpieces[TargetIndex];
+
+                    //set the movement
+                    string directionMove = move[1];
+
+                    Vector2 direction = Vector2.Zero;
+
+                    Vector2 targetPos = TargetMovePiece.Pos;
+
+                    if (directionMove == "left")
+                    {
+                        direction = new Vector2(-1, -1);
+                        Console.WriteLine(targetPos);
+                    }
+
+                    else if (directionMove == "right")
+                    {
+                        direction = new Vector2(1, -1);
+                        Console.WriteLine(targetPos);
+                    }
+
+
+                   
+                    //if the player has moved check to see if the movement will make the piece go out of bounds
+                    if (direction != Vector2.Zero)
+                    {
+                        Vector2 targetPosition = TargetMovePiece.Pos + direction;
+
+                        //the player has now moved a piece. Change to player O's turn
+                        turn = 1;
+
+                        //if the player has moved check to see if the movement will make the piece go out of bounds
+                        if (targetPos.X >= 0 || targetPos.X < boardSize && targetPos.Y >= 0 || targetPos.Y < boardSize)
+                        {
+                            TargetMovePiece.Pos = targetPos;
+
+                            if (targetPosition.X >= 0 && targetPosition.X < boardSize
+                            && targetPosition.Y >= 0 && targetPosition.Y < boardSize)
+                            {
+                                Piece occupyingPiece = null;
+
+                                foreach (Piece piece in Opieces)
+                                {
+                                    if (piece.Pos == targetPosition)
+                                    {
+                                        occupyingPiece = piece;
+
+                                        break;
+                                    }
+                                }
+
+                                if (occupyingPiece == null)
+                                {
+                                    TargetMovePiece.Pos += direction;
+                                }
+
+                                else
+                                {
+                                    //if the spot is occupied by an O piece; calculate the movement to make to check to see if it will result in a piece moving off the board
+                                    Vector2 CaptureTarget = TargetMovePiece.Pos + (direction + direction);
+
+                                    //if it does only make it take up the spot the captured piece was on
+                                    if (CaptureTarget.X <= 0 || CaptureTarget.X >= boardSize && CaptureTarget.Y <= 0 || CaptureTarget.Y >= boardSize)
+                                    {
+                                        TargetMovePiece.Pos += direction;
+                                    }
+
+                                    //if not then jump
+                                    else
+                                    {
+                                        TargetMovePiece.Pos = CaptureTarget;
+                                    }
+
+                                    //remove captured piece
+                                    Opieces.Remove(occupyingPiece);
+                                }
+                            }
+                        }
+                        continue;
+                    }
+                }
+
+
+                //this is basically the exact same thing as X but swapped to O
+                if (turn == 1)
+                {
+                    Console.WriteLine("Player O turn");
+                    string moveinput = Console.ReadLine();
+                    string[] move = moveinput.Split(' ');
+
+
+
+                    int TargetIndex = -1;
+
+                    string movePiece = move[0];
+                    if (movePiece.Length < 2)
+                    {
+                        Console.WriteLine("Invalid move format. Please enter a valid move.");
+                        continue;
+                    }
+
+                    int Xtarget = BoardPlaces[movePiece[0]];
+                    int YTargetSubtract = Convert.ToInt32(movePiece.Substring(1));
+                    int Ytarget = boardSize - YTargetSubtract;
+
+                    Console.WriteLine($"Move Piece: {movePiece}, Xtarget: {Xtarget}, Ytarget: {Ytarget}");
+
+                    Vector2 TargetPiecePos = new Vector2(Xtarget, Ytarget);
+
+                    for (int i = 0; i < Opieces.Count; i++)
+                    {
+                        if (Opieces[i].Pos == new Vector2(Xtarget, Ytarget))
+                        {
+                            TargetIndex = i;
+                            break;
+                        }
+                    }
+
+                    if (TargetIndex == -1)
+                    {
+                        Console.WriteLine("Invalid move. Please enter a valid move.");
+                        continue;
+                    }
+
+                    Piece TargetMovePiece = Opieces[TargetIndex];
+
+                    string directionMove = move[1];
+
+                    Vector2 direction = Vector2.Zero;
+
+                    Vector2 targetPos = TargetMovePiece.Pos;
+
+                    //go down rather than up
+                    if (directionMove == "left")
+                    {
+                        direction = new Vector2(-1, 1);
+                        Console.WriteLine(targetPos);
+                    }
+
+                    else if (directionMove == "right")
+                    {
+                        direction = new Vector2(1, 1);
+                        Console.WriteLine(targetPos);
+                    }
+
+
+
+
+
+                    if (direction != Vector2.Zero)
+                    {
+                        Vector2 targetPosition = TargetMovePiece.Pos + direction;
+
+                        turn = 0;
+
+                        if (targetPos.X >= 0 || targetPos.X < boardSize && targetPos.Y >= 0 || targetPos.Y < boardSize)
+                        {
+                            TargetMovePiece.Pos = targetPos;
+
+                            if (targetPosition.X >= 0 && targetPosition.X < boardSize
+                            && targetPosition.Y >= 0 && targetPosition.Y < boardSize)
+                            {
+                                Piece occupyingPiece = null;
+
+                                foreach (Piece piece in Xpieces)
+                                {
+                                    if (piece.Pos == targetPosition)
+                                    {
+                                        occupyingPiece = piece;
+
+                                        break;
+                                    }
+                                }
+
+                                if (occupyingPiece == null)
+                                {
+                                    TargetMovePiece.Pos += direction;
+                                }
+
+                                else
+                                {
+                                    Vector2 CaptureTarget = TargetMovePiece.Pos + (direction + direction);
+                                    if (CaptureTarget.X <= 0 || CaptureTarget.X >= boardSize && CaptureTarget.Y <= 0 || CaptureTarget.Y >= boardSize)
+                                    {
+                                        TargetMovePiece.Pos += direction;
+                                    }
+
+                                    else
+                                    {
+                                        TargetMovePiece.Pos = CaptureTarget;
+                                    }
+
+                                    Xpieces.Remove(occupyingPiece);
+                                }
+                            }
+                        }
+                        continue;
+                    }
+                }
+
+
+
+                //win conditions
+                if (Opieces.Count == 0)
+                {
+                    Console.Clear();
+                    PrintBoard(Xpieces, Opieces, boardSize);
+                    Console.WriteLine("Game over! X wins!");
+                    break;
+                } 
+
+                else if (Xpieces.Count == 0)
+                {
+                    Console.Clear();
+                    PrintBoard(Xpieces, Opieces, boardSize);
+                    Console.WriteLine("Game over! O wins!");
+                    break;
+                }
+
+                //continue with the game if no one wins
+                else
+                {
+                    continue;
+                }
+            }
+
+        }
+
+
+            static void PrintBoard(List<Piece> Xpieces, List<Piece> Opieces, int boardSize)
+            {
+
+            //Print out the corrdinate system
+            char[] header = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+            Console.Write("  ");
+            for (int i = 0; i < header.Length; i++)
+            {
+                Console.Write($" {header[i]} ");
+            }
+
+            Console.WriteLine();
+                
+                for (int y = 0; y < boardSize; y++)
+                {
+                //Print the number for the row (its in reverse so math time)
+                Console.Write($"{boardSize - y} ");
+
+                    for (int x = 0; x < boardSize; x++)
+                    {
+                        bool isOccupied = false;
+
+                        Console.Write("[");
+
+                        //if a piece is at the location of the tile, print the piece's team
+                        foreach (Piece piece in Xpieces)
+                        {
+                            if (piece.Pos == new Vector2(x, y))
+                            {
+                                Console.Write(piece.Team);
+
+                                isOccupied = true;
+                                break;
+                            }
+                        }
+
+                        //if a piece is at the location of the tile, print the piece's team
+                        foreach (Piece piece in Opieces)
+                        {
+                            if (piece.Pos == new Vector2(x, y))
+                            {
+                                Console.Write(piece.Team);
+
+                                isOccupied = true;
+                                break;
+                            }
+                        }
+
+                        if (isOccupied == false)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        Console.Write("]");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
---
+++ CodingChallenges/CodingChallenges.cs
@@ -0,0 +1,246 @@
+﻿namespace Assignment1
+{
+    abstract class Piece
+    {
+        public Vector2 Pos;
+        public char Team;
+
+        public Piece (char team)
+        {
+            Team = team;
+        }
+
+        public abstract List<Vector2> CalculateMovements();
+    }
+
+    class Knight : Piece
+    {
+        public Knight(char team) : base (team) 
+        {
+        }
+
+        public override List<Vector2> CalculateMovements()
+        {
+            List<Vector2> movements =
+            [
+                Pos + new Vector2(1, -2),
+                Pos - new Vector2(2, -1),
+                Pos + new Vector2(2, -1),
+                Pos - new Vector2(1, -2),
+                Pos + new Vector2(1, 2),
+                Pos - new Vector2(2, 1),
+                Pos + new Vector2(2, 1),
+                Pos - new Vector2(1, 2),
+            ];
+
+            return movements;
+        }
+    }
+
+    struct Vector2
+    {
+        public int X, Y;
+
+        public static readonly Vector2 Zero = new Vector2(0,0);
+
+        public Vector2(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X + b.X, a.Y + b.Y);
+        }
+
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.X - b.X, a.Y - b.Y);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{X}, {Y}";
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            int boardSize = 8;
+            List<Piece> = new List<Piece>();
+            piece player = new Knight('X');
+            List<Vector2> movements = new List<Vector2>();
+
+            PrintBoard(Piece, boardSize);
+
+            while (true)
+            {
+                Console.Clear();
+                
+                List<Vector2> movements = player.CalculateMovements();
+                
+
+                PrintBoard(Piece, boardSize);
+                while (!Console.KeyAvailable) { }
+                ConsoleKey key = Console.ReadKey(true).Key;
+                Vector2 targetPos = Piece.Pos;
+
+                if (key == ConsoleKey.Q)
+                {
+                    targetPos.Y -= 1;
+                    targetPos.X -= 1;
+                    Console.WriteLine(targetPos);
+                }
+
+                else if (key == ConsoleKey.E)
+                {
+                    targetPos.Y -= 1;
+                    targetPos.X += 1;
+                    Console.WriteLine(targetPos);
+                }
+
+                else if (key == ConsoleKey.A)
+                {
+                    targetPos.Y += 1;
+                    targetPos.X -= 1;
+                    Console.WriteLine(targetPos);
+                }
+
+                else if (key == ConsoleKey.D)
+                {
+                    targetPos.Y += 1;
+                    targetPos.X += 1;
+                    Console.WriteLine(targetPos);
+                }
+                if (targetPos.X >=0 && targetPos.X < boardSize && targetPos.Y >= 0 && targetPos.Y < boardSize)
+                {
+                    Piece.Pos = targetPos;
+                }
+            }
+
+        }
+        static void PrintBoard(Piece piece, int N)
+        {
+            string TilePiece = "[X]";
+            string tile = "[ ]";
+            
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (piece.Pos.Y == i && piece.Pos.X == j)
+                    {
+                        Console.Write(TilePiece);
+                    }
+                    else
+                    {
+                        Console.Write(tile);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(piece.Pos);
+
+        }
+    }
+
+   
+    //internal class GuessingGame
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Vector2 location = new Vector2(0.0f, 0.0f);
+
+    //        Character character = new Character(location, "cube");
+    //        character.translateDistance = new Vector2(1.5f, 2.5f);
+    //        character.position = character.Translate(character.translateDistance, character.position);
+    //        Console.WriteLine(character.position);
+            
+           
+
+
+            //        Console.WriteLine("Enter the Maximum number");
+            //        int num = Convert.ToInt32(Console.ReadLine());
+            //        Random rand = new Random();
+            //        int Answer = rand.Next(1, num + 1);
+
+            //        bool runtime = true;
+            //        while (runtime)
+            //        {
+            //            Console.WriteLine($"Guess a number between 1 and {num}");
+            //            int guess = Convert.ToInt32(Console.ReadLine());
+
+            //            if (guess == Answer)
+            //            {
+            //                Console.WriteLine("Correct guess!");
+            //                runtime = false;
+            //            }
+
+            //            else if (guess > Answer)
+            //            {
+            //                Console.WriteLine("Guess too high, try again");
+            //            }
+
+            //            else if (guess < Answer)
+            //            {
+            //                Console.WriteLine("Guess too low, try again");
+            //            }
+
+            //        }
+
+            //    }
+            //}
+
+            //string text = "The quick brown fox jumps over the lazy dog";
+            //bool run = true;
+            //int loop = 0;
+            //while (run)
+            //{
+            //    if (loop == 0)
+            //    {
+            //        Console.Clear();
+            //        for (int i = 0; i < text.Length; i++)
+            //        {
+            //            Console.Write(text[i]);
+            //            Thread.Sleep(100);
+            //        }
+            //        Console.WriteLine();
+
+            //        Console.WriteLine("Enter a letter");
+            //        loop++;
+            //    }
+
+            //    else
+            //    {
+            //        string letterString = Console.ReadLine();
+            //        char LetterChar = Convert.ToChar(letterString);
+            //        Console.Clear();
+            //        for (int i = 0; i < text.Length; i++)
+            //        {
+            //            if (text[i] == LetterChar)
+            //            {
+            //                Console.Write(char.ToUpper(text[i]));
+            //                Thread.Sleep(100);
+            //            }
+
+            //            else
+            //            {
+            //                Console.Write(text[i]);
+            //                Thread.Sleep(100);
+            //            }
+            //        }
+            //        Console.WriteLine();
+
+            //        Console.WriteLine("Enter a letter");
+            //    }
+            //}
+
+
+
+               
+        //    }
+        //}
+    }
